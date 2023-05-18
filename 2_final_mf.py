cc4808395114deae64156bb236a6c1fa5e7a03f9
Subir Saha <subirsaha@Subirs-MacBook-Air.local>
2023-05-19 00:38:37 +0530

    add log in code

--- 2_final_mf.py
+++ 2_final_mf.py
@@ -5,10 +5,27 @@ from datetime import datetime, timedelta,date
 import warnings 
 warnings.simplefilter(action='ignore')
 import openpyxl
+import configparser
+from cryptography.fernet import Fernet
+from loguru import logger
+logger.add('all_fund_ID.log', rotation='10 MB')
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 # pd.set_option('display.max_columns', None)
 
-db = mysql.connect(host = 'localhost', port = 3306, user = 'root', password='Bappa@1234567', database='Stock2023')
+key = config['mysqldb']['key']
+cipher_suite = Fernet(key.encode('utf-8'))
+# ciphered_text = cipher_suite.encrypt(config['mysqldb']['db_password'])   
+
+unciphered_pwd = (cipher_suite.decrypt((config['mysqldb']['db_password']).encode('utf-8'))).decode()
+
+myhost = config['mysqldb']['host']
+print(myhost)
+db = mysql.connect(host = 'localhost', port = config['mysqldb']['port'], \
+    user = config['mysqldb']['user'], password=unciphered_pwd, database= 'STOCK2023')
 cursor = db.cursor()
 query = "select * from mf_fund3"
 
@@ -29,6 +46,7 @@ mf_growth['Date'] = pd.to_datetime(mf_growth['Date'])
 
 
 all_fund_ID = mf_growth.loc[(mf_growth['Date']==(pd.Timestamp(max(mf_growth['Date']))))].Scheme_Code.unique()
+logger.info(f"extracting data for {all_fund_ID}")
 
 return_df = pd.DataFrame(columns=[
     'Scheme_Code', 'Scheme_Name', 'Net_Asset_Value', 'Date', '3M_OLD_Price',
@@ -41,33 +59,35 @@ count = 0
 
 for fund in all_fund_ID:
     count = count+1
-    union_liquid_mf = mf_growth[mf_growth['Scheme_Code'].str.contains(fund)]
-    print(f'fatching data for {count}: {union_liquid_mf.Scheme_Name.unique()}')
+    all_liquid_mf = mf_growth[mf_growth['Scheme_Code'].str.contains(fund)]
+    print(f'fatching data for {count}: {all_liquid_mf.Scheme_Name.unique()}')
+    logger.info(f"extracting data for {all_liquid_mf}")
 
-    start_date = pd.Timestamp(max(union_liquid_mf['Date']))
+    start_date = pd.Timestamp(max(all_liquid_mf['Date']))
     end_date = start_date + pd.Timedelta(days=-1825)
     # print(start_date,end_date)
-    union_mf_5Y = union_liquid_mf.loc[(union_liquid_mf['Date'] <= start_date) & (union_liquid_mf['Date'] >= end_date), :]
+    all_mf_5Y = all_liquid_mf.loc[(all_liquid_mf['Date'] <= start_date) & (all_liquid_mf['Date'] >= end_date), :]
     # print(max(union_liquid_mf['Date']),min(union_liquid_mf['Date']))
 
-    union_mf_5Y.sort_values(['Date'],ascending=[0],inplace=True)
+    all_mf_5Y.sort_values(['Date'],ascending=[0],inplace=True)
 
-    union_mf_5Y['3M_OLD_Price'] = union_mf_5Y['Net_Asset_Value'].shift(-63)
-    union_mf_5Y['3M_per_chng']=((union_mf_5Y['Net_Asset_Value']/union_mf_5Y['3M_OLD_Price'])**(12/3)-1)*100
-    union_mf_5Y['6M_OLD_Price'] = union_mf_5Y['Net_Asset_Value'].shift(-126)
-    union_mf_5Y['6M_per_chng']=((union_mf_5Y['Net_Asset_Value']/union_mf_5Y['6M_OLD_Price'])**(12/6)-1)*100
-    union_mf_5Y['1Y_OLD_Price'] = union_mf_5Y['Net_Asset_Value'].shift(-252)
-    union_mf_5Y['1Y_per_chng']=((union_mf_5Y['Net_Asset_Value']/union_mf_5Y['1Y_OLD_Price'])**(12/12)-1)*100
-    union_mf_5Y['3Y_OLD_Price'] = union_mf_5Y['Net_Asset_Value'].shift(-756)
-    union_mf_5Y['3Y_per_chng'] = ((union_mf_5Y['Net_Asset_Value']/union_mf_5Y['3Y_OLD_Price'])**(12/36)-1)*100
-    union_mf_5Y['5Y_OLD_price'] = union_mf_5Y['Net_Asset_Value'].shift(-1260)
-    union_mf_5Y['5Y_per_chng'] = ((union_mf_5Y['Net_Asset_Value']/union_mf_5Y['5Y_OLD_price'])**(12/60)-1)*100
+    all_mf_5Y['3M_OLD_Price'] = all_mf_5Y['Net_Asset_Value'].shift(-63)
+    all_mf_5Y['3M_per_chng']=((all_mf_5Y['Net_Asset_Value']/all_mf_5Y['3M_OLD_Price'])**(12/3)-1)*100
+    all_mf_5Y['6M_OLD_Price'] = all_mf_5Y['Net_Asset_Value'].shift(-126)
+    all_mf_5Y['6M_per_chng']=((all_mf_5Y['Net_Asset_Value']/all_mf_5Y['6M_OLD_Price'])**(12/6)-1)*100
+    all_mf_5Y['1Y_OLD_Price'] = all_mf_5Y['Net_Asset_Value'].shift(-252)
+    all_mf_5Y['1Y_per_chng']=((all_mf_5Y['Net_Asset_Value']/all_mf_5Y['1Y_OLD_Price'])**(12/12)-1)*100
+    all_mf_5Y['3Y_OLD_Price'] = all_mf_5Y['Net_Asset_Value'].shift(-756)
+    all_mf_5Y['3Y_per_chng'] = ((all_mf_5Y['Net_Asset_Value']/all_mf_5Y['3Y_OLD_Price'])**(12/36)-1)*100
+    all_mf_5Y['5Y_OLD_price'] = all_mf_5Y['Net_Asset_Value'].shift(-1260)
+    all_mf_5Y['5Y_per_chng'] = ((all_mf_5Y['Net_Asset_Value']/all_mf_5Y['5Y_OLD_price'])**(12/60)-1)*100
 
-    union_df = union_mf_5Y.iloc[:1]
+    all_df = all_mf_5Y.iloc[:1]
+    logger.info(f"extracting data for {all_df}")
     # print(union_df)
     # break
     # return_df = return_df.append(union_df,ignore_index=True)
-    return_df = pd.concat([return_df,union_df])
+    return_df = pd.concat([return_df,all_df])
 
 
 # print(return_df.head())
